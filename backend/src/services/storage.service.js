272adb956d1c01f21b4ae8e8607167885ec3225a
Sarvam Taneja <sarvamtaneja@gmail.com>
2026-04-13 20:35:34 +0530

    Initial commit

---
+++ backend/src/services/storage.service.js
@@ -0,0 +1,19 @@
+const { ImageKit } = require("@imagekit/nodejs")
+
+
+const ImageKitClient = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+})
+
+async function uploadFile(file){
+    const result = await ImageKitClient.files.upload({
+        file,
+        filename:"music_"+ Date.now(),
+        folder:"Spotify_clone/music"
+    })
+
+    return result;
+}
+
+
+module.exports = {uploadFile};
